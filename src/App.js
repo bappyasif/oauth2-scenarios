636dc3b3ce10aea82f17dcf6d489ac91ee447b82
bappyasif <bappyasif@hotmail.com>
2023-01-09 01:31:41 +0600

    initial commit
    * trying out oauth2 with express session

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Navbar } from './components/Navbar';
+import Home from './routes/Home';
+import { Login } from './routes/Login';
 
 function App() {
+  let [user, setUser] = useState()
+  useEffect(() => {
+    fetch("http://localhost:4000/auth/login/success", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true
+      }
+    })
+    .then(resp => resp.status === 200 && resp.json())
+    .catch(err=>console.log(err, "request error!!"))
+    .then(data => console.log(data))
+    .catch(err=>console.log(err, "response error!!"))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/routes/Login.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export const Login = () => {
+    const google = () => {
+        window.open("http://localhost:4000/auth/google", "_self");
+    };
+
+    const github = () => {
+        window.open("http://localhost:4000/auth/github", "_self");
+    };
+
+    const facebook = () => {
+        window.open("http://localhost:4000/auth/facebook", "_self");
+    };
+
+    return (
+        <div className="login">
+            <h1 className="loginTitle">Choose a Login Method</h1>
+            <div className="wrapper">
+                <div className="left">
+                    <div className="loginButton google" onClick={google}>
+                        Google
+                    </div>
+                    <div className="loginButton facebook" onClick={facebook}>
+                        Facebook
+                    </div>
+                    <div className="loginButton github" onClick={github}>
+                        Github
+                    </div>
+                </div>
+                <div className="center">
+                    <div className="line" />
+                    <div className="or">OR</div>
+                </div>
+                <div className="right">
+                    <input type="text" placeholder="Username" />
+                    <input type="text" placeholder="Password" />
+                    <button className="submit">Login</button>
+                </div>
+            </div>
+        </div>
+    );
+};
